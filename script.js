b1b2dde826d4a8f1c28ddf2d8e276f37ba446a56
Lamia BOURICH <lamiascience034@gmail.com>
2022-10-31 17:29:47 +0100

    hitaka

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ window.onscroll = () => {
 	menu.classList.remove('bx-x');u
 	navbar.classList.remove('open');
 };
+/*
 //add to card 
 var addItemId = 0;
  function addToCart(item){
@@ -28,3 +29,36 @@ var addItemId = 0;
 	selectedItem.append(img1);
 	chosenItems.append(selectedItem);
  }
+ */
+
+ //second try add to card
+ /*
+ var li_element = document.querySelectorAll(".category option");
+var item_elements = document.querySelectorAll(".card");
+ for(var i = 0;i < option_elements.length; i++){
+	option_elements[i].addEventListener("click",function(){
+		option_elements.forEach(function(option)){
+			option.classList.remove("active");
+		}
+		this.classList.add("active");
+		var option_value = this.getAttribute("data-option");
+		item_elements.forEach(function(item)
+			item.style.display = "none";
+	})
+		if(option_value == "Sushis"){
+			document.querySelector("." + option_value).getElementsByClassName.display = "block";
+		}
+		else if(option_value == "Salades"){
+			document.querySelector("." + option_value).getElementsByClassName.display = "block";
+		}
+		else if(option_value == "Desserts"){
+			document.querySelector("." + option_value).getElementsByClassName.display = "block";
+		}
+		else if(option_value == "Drinks"){
+			document.querySelector("." + option_value).getElementsByClassName.display = "block";
+		}
+		else{
+			consolelog("");
+		}
+	})
+ }*/
